525c6b462a7bd5b476ce4596a436994734c5c764
Aryan rathour <aryanrathour066@gmail.com>
2026-04-11 14:23:30 +0530

    ..

--- hooks/useGet.ts
+++ hooks/useGet.ts
@@ -1,15 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, type QueryKey, type UseQueryOptions } from "@tanstack/react-query";
 import { apiClient } from "@/lib/apiClient";
 
-export const useGet = <T>(key: string, url: string, options?: any) => {
-
+export const useGet = <T>(
+  key: QueryKey,
+  url: string,
+  options?: Omit<UseQueryOptions<T>, "queryKey" | "queryFn">
+) => {
   return useQuery<T>({
-    queryKey: [key],
+    queryKey: key,
     queryFn: async () => {
       const { data } = await apiClient.get(url);
       return data;
     },
     ...options,
   });
-
 };
